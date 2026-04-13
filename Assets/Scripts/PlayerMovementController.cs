2ec9c20d0100313182df5f483b631c154e0cbf0b
Alyssa Bleyl <ableyl@colgate.edu>
2026-04-13 16:02:47 +0200

    better camera movement

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -2,31 +2,6 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    
-
-
-    // public float moveSpeed = 5f;
-
-    // private CharacterController controller;
-
-    // void Start()
-    // {
-    //     controller = GetComponent<CharacterController>();
-    // }
-
-    // void Update()
-    // {
-    //     float moveX = Input.GetAxis("Horizontal");
-    //     float moveZ = Input.GetAxis("Vertical");
-
-    //     Vector3 move =
-    //         transform.right * moveX +
-    //         transform.forward * moveZ;
-
-    //     controller.Move(
-    //         move * moveSpeed * Time.deltaTime
-    //     );
-    // }
 
     public float moveSpeed = 5f;
 
@@ -37,10 +12,6 @@ public class PlayerMovementController : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
-
-        // Lock cursor to center
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
 
     void Update()
@@ -48,26 +19,18 @@ public class PlayerMovementController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-   
         Vector3 forward = cameraTransform.forward;
         Vector3 right = cameraTransform.right;
 
-        
         forward.y = 0f;
         right.y = 0f;
-
         forward.Normalize();
         right.Normalize();
 
-        Vector3 moveDirection =
-            forward * moveZ +
-            right * moveX;
+        Vector3 moveDirection = forward * moveZ + right * moveX;
 
-        controller.Move(
-            moveDirection * moveSpeed * Time.deltaTime
-        );
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
 
-  
         if (moveDirection.magnitude > 0.1f)
         {
             transform.forward = moveDirection;
