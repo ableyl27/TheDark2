66dc71def317c24e0c52adb135a7bd03b1436e27
Seth Snower <sethsnower3@gmail.com>
2026-04-13 20:23:47 +0200

    quick changes

--- Assets/Scripts/PlayerInteractionController.cs
+++ Assets/Scripts/PlayerInteractionController.cs
@@ -1,24 +1,24 @@
 using UnityEngine;
+using System;
 
 public class PlayerInteractionController : MonoBehaviour
 {
-    private GameObject key;
-    
-    [SerializedField] private float InteractionRadius;
+    private GameObject keyInHand; 
+
+    [SerializeField] private float interactionRadius;
     [SerializeField] private LayerMask interactableLayers;
-
-    private Collider GetInteractable()
-    {
-       return Physics.OverlapSphere(transform.position, interactRadius, interactableLayers);
-
-    }
 
     public static event Action<string> OnInteractableFound;
 
+    private Collider[] GetInteractable()
+    {
+        return Physics.OverlapSphere(transform.position, interactionRadius, interactableLayers);
+    }
+
     private void OnInteract()
     {
-        Debug.Log("Test Interaction")
-        if(isHoldingKey())
+        Debug.Log("Test Interaction");
+        if (isHoldingKey())
         {
             DropKey();
             return;
@@ -32,13 +32,33 @@ public class PlayerInteractionController : MonoBehaviour
     }
 
     private void TryPickUpKey()
+    {
+        Collider hit = GetInteractable();
+        if(hit == null)
         {
-            //later
+            Debug.Log("no object found");
+            return;
         }
+        Key key = hit.GetComponent<key>();
+        if(key == null)
+        {
+            Debug.Log("no key found");
+            return;
+        }
+       
+       PickUpKey(hit.gameObject);
+
+    }
+
+    private void PickUpKey(GameObject key)
+    {
+        keyInHand = key;
+
+        key.transform.SetParent(transform);
+    }
 
     private void DropKey()
-        {
-            //later
-        }
-    
+    {
+        //later
+    }
 }
