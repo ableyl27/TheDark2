2b0263aebc675373810a18eae55504af8cd8847c
Alyssa Bleyl <ableyl@colgate.edu>
2026-04-14 18:55:46 +0200

    Player damage controller + health bar

--- Assets/Scripts/PlayerDamageController.cs
+++ Assets/Scripts/PlayerDamageController.cs
@@ -1,16 +1,33 @@
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerDamageController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float playerHealth = 100f;
+    [SerializeField] private Scrollbar healthBar;
+
+    public void TakeDamage(int damage)
     {
-        
+        Debug.Log("Enemy Collision");
+        playerHealth -= damage;
+        healthBar.size = playerHealth/100f;
+        if (playerHealth <= 0)
+         {
+            Debug.Log("Player died");
+         }
+            
+    
+    }
+    
+
+    void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(10);
+        }
     }
 }
