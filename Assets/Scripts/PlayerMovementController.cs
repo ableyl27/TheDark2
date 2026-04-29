f1f9e01ecdf0932e7f74c5e6fbaa1a295a6f00d3
Alyssa Bleyl <ableyl@colgate.edu>
2026-04-29 21:56:21 +0200

    new map (until maze), first enemy set up, flashlight not working

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -2,66 +2,62 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
-
     public float mouseSensitivity = 200f;
-    private float xRotation = 0f;
-
     public Transform cameraTransform;
 
     private CharacterController controller;
+    private float xRotation = 0f;
+
+    
+    public float gravity = -9.81f;
+    private Vector3 velocity; 
+ 
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
     {
+      
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
         Vector3 forward = cameraTransform.forward;
         Vector3 right = cameraTransform.right;
-
         forward.y = 0f;
         right.y = 0f;
         forward.Normalize();
         right.Normalize();
 
         Vector3 moveDirection = forward * moveZ + right * moveX;
-
         controller.Move(moveDirection * moveSpeed * Time.deltaTime);
 
+        
+        if (controller.isGrounded && velocity.y < 0)
+        {
+           
+            velocity.y = -2f; 
+        }
+
+       
+        velocity.y += gravity * Time.deltaTime;
+
+        
+        controller.Move(velocity * Time.deltaTime);
+
+       
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
 
-        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // eyes tilts up/down
-        transform.Rotate(Vector3.up * mouseX); // whole player rotates left/right
-
-         // AI
-         /*
-        if (moveDirection.magnitude > 0.1f)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 3f * Time.deltaTime);
-        }
-        */
-        }
-        // AI
-
-        //old
-        /*
-        if (moveDirection.magnitude > 0.1f)
-        {
-        transform.forward = moveDirection;
-        }
-        */
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
 }
