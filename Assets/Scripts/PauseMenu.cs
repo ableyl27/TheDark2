4f1faa781ae895f12a116b14e989af4250898353
Alyssa Bleyl <ableyl@colgate.edu>
2026-04-30 02:27:54 +0200

    start, pause, game over menus connected

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void ResumeButton()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
--- Assets/Scripts/PlayerDamageController.cs
+++ Assets/Scripts/PlayerDamageController.cs
@@ -2,24 +2,63 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerDamageController : MonoBehaviour
 {
 
     [SerializeField] private float playerHealth = 100f;
     [SerializeField] private Scrollbar healthBar;
+    [SerializeField] private GameObject gameOverMenu;
 
+    private bool isDead = false;
+
+    void Start()
+    {
+        if (gameOverMenu != null)
+        {
+            gameOverMenu.SetActive(false);
+        }
+    }
+
+    
     public void TakeDamage(int damage)
     {
+
+        if (isDead)
+        {
+            return;
+        }
         Debug.Log("Enemy Collision");
         playerHealth -= damage;
         healthBar.size = playerHealth/100f;
         if (playerHealth <= 0)
          {
-            Debug.Log("Player died");
+            Die();
+
          }
             
     
+    }
+
+    void Die()
+    {
+        isDead = true;
+        Debug.Log("Player died");
+        if (gameOverMenu != null)
+        {
+            gameOverMenu.SetActive(true);
+            Time.timeScale = 0f;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     
 
