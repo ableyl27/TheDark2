b10822949babf95219220b0bb402062834239295
Seth Snower <sethsnower3@gmail.com>
2026-05-02 19:26:13 +0200

    ending

--- Assets/Scripts/PickupUI.cs
+++ Assets/Scripts/PickupUI.cs
@@ -7,6 +7,9 @@ public class PickupUI : MonoBehaviour
     [SerializeField] private GameObject pickupUI;
     [SerializeField] private GameObject keyIndicator;
 
+    [SerializeField] private GameObject chestOpenedPanel;
+    [SerializeField] private GameObject wrongKeyPanel;
+
     private bool hasPickedUpKey = false;
 
     private void OnEnable()
@@ -15,6 +18,8 @@ public class PickupUI : MonoBehaviour
         PlayerInteractionController.OnKeyPickedUp += ShowKeyIndicator;
         PlayerInteractionController.OnKeyDropped += HideKeyIndicator;
         PlayerInteractionController.OnKeyUsed += HideKeyIndicator;
+        chest.OnChestOpened += ShowChestOpened;
+        chest.OnWrongKey += ShowWrongKey;
     }
 
     private void OnDisable()
@@ -23,6 +28,27 @@ public class PickupUI : MonoBehaviour
         PlayerInteractionController.OnKeyPickedUp -= ShowKeyIndicator;
         PlayerInteractionController.OnKeyDropped -= HideKeyIndicator;
         PlayerInteractionController.OnKeyUsed -= HideKeyIndicator;
+        chest.OnChestOpened -= ShowChestOpened;
+        chest.OnWrongKey -= ShowWrongKey;
+    }
+
+    private void ShowChestOpened()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ShowPanelRoutine(chestOpenedPanel));
+    }
+
+    private void ShowWrongKey()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ShowPanelRoutine(wrongKeyPanel));
+    }
+
+    private IEnumerator ShowPanelRoutine(GameObject panel)
+    {
+        panel.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        panel.SetActive(false);
     }
 
     private void ShowKeyIndicator()
---
+++ Assets/Scripts/chest.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System;
+
+public class chest : MonoBehaviour, IInteractable
+{
+    [Header("Settings")]
+    [SerializeField] private string requiredKeyTag;
+
+    [Header("Animation")]
+    [SerializeField] private Animator chestAnimator;
+
+    [Header("Audio")]
+    [SerializeField] private AudioClip correctKeySound;
+    [SerializeField] private AudioClip wrongKeySound;
+    [SerializeField] private AudioSource audioSource;
+
+    public static event Action OnChestOpened;
+    public static event Action OnWrongKey;
+
+    private bool isOpen = false;
+
+    public void Interact(PlayerInteractionController player)
+    {
+        if (isOpen) return;
+
+        if (player.keyInHand != null && player.keyInHand.CompareTag(requiredKeyTag))
+        {
+            isOpen = true;
+            chestAnimator.SetBool("isOpen", true);
+            Destroy(player.keyInHand);
+            player.keyInHand = null;
+            player.UseKey();
+            audioSource.PlayOneShot(correctKeySound);
+            OnChestOpened?.Invoke();
+        }
+        else
+        {
+            audioSource.PlayOneShot(wrongKeySound);
+            OnWrongKey?.Invoke();        
+        }
+    }
+
+    public string GetInteractText(PlayerInteractionController player)
+    {
+        if (isOpen) return "";
+
+        if (player.keyInHand != null && player.keyInHand.CompareTag(requiredKeyTag))
+            return "Press E to open chest";
+
+        return "Locked (need key)";
+    }
+}
